94820e1362d40a3e70cc698747eceb83fa19a517
T.D. Stoneheart <tduyduc@ymail.com>
2020-02-07 20:26:24 +0700

    Implement certain classes

--- classes.ts
+++ classes.ts
@@ -1,9 +1,25 @@
 /// <reference path="definitions.ts" />
 
 /**
+ * Contains common utility functions.
+ *
+ * @class
+ * @static
+ */
+class Common {
+  static randomBool(): boolean {
+    return !!Math.round(Math.random());
+  }
+
+  static randomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+}
+
+/**
  * Represents in-game character position in 2D.
  *
- * @interface
+ * @class
  */
 class CharacterPosition implements IPosition {
   x: number = 0;
@@ -23,5 +39,79 @@ class CharacterPosition implements IPosition {
 class ManhattanDistance implements IDistanceFunction {
   static calculate(a: CharacterPosition, b: CharacterPosition) {
     return Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
+  }
+}
+
+class DefaultDamage implements IDamageFunction {
+  static calculate(
+    originalDamage: number,
+    defense: number,
+    variation: number = 0
+  ) {
+    const damageMultiplier =
+      defense >= 0 ? 100 / (100 + defense) : 2 - 100 / (100 - defense);
+    const variedDamage =
+      Math.floor(Math.random() * variation) * (Common.randomBool() ? 1 : -1);
+    return originalDamage * damageMultiplier + variedDamage;
+  }
+}
+
+class Attributes implements IAttributes {
+  hp: number;
+  mp: number;
+  attack: number;
+  defense: number;
+  intelligence: number;
+  mind: number;
+  attackRange: number;
+  attackArea: number;
+  speed: number;
+  movementRange: number;
+  time: number;
+  position: CharacterPosition;
+}
+
+class AttributeDisplayObject implements IAttributeDisplayObject {
+  name: string;
+  value: any;
+  constructor(name: string, value: any) {
+    this.name = name;
+    this.value = value;
+  }
+}
+
+class AttributesDisplay {
+  /**
+   * Friendly names of attributes, to display.
+   * @static
+   * @member {object}
+   */
+  static friendlyNames = {
+    hp: "HP",
+    mp: "MP",
+    attack: "Attack",
+    defense: "Defense",
+    intelligence: "Int.",
+    mind: "Mind",
+    attackRange: "Range",
+    attackArea: "Area",
+    speed: "Speed",
+    movementRange: "Move"
+  };
+
+  /**
+   * Generate an array of attributes, with display names.
+   * @static
+   * @method AttributeDisplayObject[]
+   */
+  static generate(attributes: Attributes): AttributeDisplayObject[] {
+    const result = [];
+    for (const field in this.friendlyNames) {
+      if (!(field in attributes)) continue;
+      result.push(
+        new AttributeDisplayObject(this.friendlyNames[field], attributes[field])
+      );
+    }
+    return result;
   }
 }
--- definitions.ts
+++ definitions.ts
@@ -4,13 +4,45 @@
  * @interface IGlobalConfig
  */
 interface IGlobalConfig {
+  /** Milliseconds between event ticks.
+   * @member {number}
+   */
   battleSpeed: number;
+
+  /** Playing mode (e.g. single player or zero player).
+   * @member {PlayMode}
+   */
   playMode: PlayMode;
+
+  /** AI mode of the ally team (applicable on AI vs. AI play mode).
+   * @member {AIMode}
+   */
   allyAIMode: AIMode;
+
+  /** AI mode of the enemy team.
+   * @member {AIMode}
+   */
   enemyAIMode: AIMode;
+
+  /** Number of character in a team.
+   * @member {number}
+   */
   teamMembers: number;
+
+  /** Displayed width and height of an in-game square, in pixels.
+   * @member {number}
+   */
+  cellSize: number;
+
+  /** Number of squares per dimension in the game board.
+   * @member {number}
+   */
   mapSize: number;
-  turnLimit: number;
+
+  /** Number of consecutive inactive turns before the game is forcefully ended.
+   * @member {number}
+   */
+  inactiveTurnLimit: number;
 }
 
 /**
@@ -19,30 +51,101 @@ interface IGlobalConfig {
  * @interface
  */
 interface IPosition {
+  /** Abscissa.
+   * @member {number}
+   */
   x: number;
+
+  /** Ordinate.
+   * @member {number}
+   */
   y: number;
 }
 
 /**
- * Interface for wrapping a function which calculates distance between two Position objects.
+ * Interface for wrapping a function which calculates distance between two IPosition objects.
  *
  * @interface
  * @static
  */
 class IDistanceFunction {
+  /** Performs calculation using the implemented function.
+   * @method number
+   * @static
+   */
   static calculate: (p1: IPosition, p2: IPosition) => number;
 }
 
 /**
- * Represents an in-game character.
+ * Wraps a string that is designated to be a file path.
  *
  * @interface
  */
-interface ICharacter {
-  name: string;
+interface IFilePath {
+  /** Wrapped file path.
+   * @member {string}
+   */
+  filePath: string;
+
+  /** Wrapped file path, as a helper function.
+   * @method string
+   */
+  toString: () => string;
+
+  /** Wrapped file path, as a helper function.
+   * @method string
+   */
+  valueOf: () => string;
+}
+
+/**
+ * Represents an in-game character class.
+ *
+ * @interface
+ */
+interface ICharacterClass {
+  /**
+   * Name of the character class.
+   * @member {string}
+   */
+  className: string;
+
+  /**
+   * Initial attributes of the class.
+   * @member {IAttributes}
+   */
   initialAttributes: IAttributes;
+
+  /**
+   * Default character names for the class.
+   * @member {string[]}
+   */
+  defaultCharacterNames?: string[];
+
+  /**
+   * File path for the default sprite image of the class.
+   * @member {IFilePath}
+   */
+  sprite: IFilePath;
+}
+
+/**
+ * Represents an in-game character. A character must be of a character class.
+ *
+ * @interface
+ */
+interface ICharacter extends ICharacterClass {
+  /**
+   * Name of the character.
+   * @member {string}
+   */
+  characterName: string;
+
+  /**
+   * Current attributes of the character.
+   * @member {IAttributes}
+   */
   inGameAttributes: IAttributes;
-  position: Position;
 }
 
 /**
@@ -51,16 +154,112 @@ interface ICharacter {
  * @interface
  */
 interface IAttributes {
+  /**
+   * Hit points. Amount of damage a character can sustain before getting knocked out, unable to take any further action.
+   * @member {number}
+   */
   hp: number;
+
+  /**
+   * Magic points. Limits how many special abilities a character can use.
+   * @member {number}
+   */
   mp: number;
+
+  /**
+   * Physical attack power. Determines amount of damage dealt with physical attacks.
+   * @member {number}
+   */
   attack: number;
+
+  /**
+   * Physical defense power. Determines amount of damage reduced with physical attacks.
+   * @member {number}
+   */
   defense: number;
+
+  /**
+   * Magical attack power. Determines amount of damage dealt with magical attacks.
+   * @member {number}
+   */
   intelligence: number;
+
+  /**
+   * Magical defense power. Determines amount of damage reduced with magical attacks.
+   * @member {number}
+   */
   mind: number;
+
+  /**
+   * Attack range. Determines maximum distance an attack can reach from the character.
+   * @member {number}
+   */
   attackRange: number;
+
+  /**
+   * Attack area. Determines maximum distance an attack can spread from the center of the damage position.
+   * @member {number}
+   */
   attackArea: number;
+
+  /**
+   * Speed. Determines how fast the ATB bar is filled.
+   * @member {number}
+   */
   speed: number;
+
+  /**
+   * Movement range. Determines maximum distance a character can move.
+   * @member {number}
+   */
   movementRange: number;
+
+  /**
+   * Time. ATB bar filling progress.
+   * @member {number}
+   */
+  time: number;
+
+  /**
+   * Position on the game board.
+   * @member {IPosition}
+   */
+  position: IPosition;
+}
+
+/**
+ * Interface for wrapping a function which calculates damage dealt on a character.
+ *
+ * @interface
+ * @static
+ */
+class IDamageFunction {
+  /** Performs calculation using the implemented function.
+   * @method number
+   * @static
+   */
+  static calculate: (
+    originalDamage: number,
+    defense: number,
+    variation?: number
+  ) => number;
+}
+
+/**
+ * Interface for displaying an attribute with its friendly name and its actual value.
+ *
+ * @interface
+ */
+interface IAttributeDisplayObject {
+  /** Attribute friendly name.
+   * @member {number}
+   */
+  name: string;
+
+  /** Attribute value.
+   * @member {any}
+   */
+  value: any;
 }
 
 /**
@@ -68,7 +267,11 @@ interface IAttributes {
  *
  * @interface
  */
-interface GameState {
+interface IGameState {
+  /**
+   * Keeps track of character waiting to get a turn
+   * @member {ICharacter[]}
+   */
   turnQueue: ICharacter[];
 }
 
