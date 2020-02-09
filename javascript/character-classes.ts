d160519d0a33a48dc84c490fdf4b5ea4c31ea178
T.D. Stoneheart <tduyduc@ymail.com>
2020-02-09 22:16:34 +0700

    Add some more character classes

--- javascript/character-classes.ts
+++ javascript/character-classes.ts
@@ -17,5 +17,56 @@ const characterClasses = [
       speed: 40,
       movementRange: 2
     })
-  })
+  }),
+  new CharacterClass({
+    className: "Black Belt",
+    sprite: "../res/sabin.gif",
+    defaultCharacterNames: ["Sabin", "Yang", "Galuf"],
+    initialAttributes: new CharacterAttributes({
+      hp: 400,
+      mp: 25,
+      attack: 70,
+      defense: 30,
+      intelligence: 10,
+      mind: 10,
+      attackRange: 1,
+      attackArea: 0,
+      speed: 25,
+      movementRange: 2
+    })
+  }),
+  new CharacterClass({
+    className: "Archer",
+    sprite: "../res/edgar.gif",
+    defaultCharacterNames: ["Edgar", "Ceodore", "Edward"],
+    initialAttributes: new CharacterAttributes({
+      hp: 300,
+      mp: 300,
+      attack: 30,
+      defense: 15,
+      intelligence: 20,
+      mind: 15,
+      attackRange: 3,
+      attackArea: 0,
+      speed: 35,
+      movementRange: 2
+    })
+  }),
+  new CharacterClass({
+    className: "Assassin",
+    sprite: "../res/shadow.gif",
+    defaultCharacterNames: ["Shadow", "Edge", "Jinnai"],
+    initialAttributes: new CharacterAttributes({
+      hp: 250,
+      mp: 20,
+      attack: 45,
+      defense: 15,
+      intelligence: 10,
+      mind: 10,
+      attackRange: 1,
+      attackArea: 0,
+      speed: 80,
+      movementRange: 3
+    })
+  }),
 ];
